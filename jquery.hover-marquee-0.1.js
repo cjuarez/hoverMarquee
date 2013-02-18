0c44321448ecc438d9231737f220466bbfb99d01
Celso Ulises Juarez Ramirez <ulyman@gmail.com>
2013-02-17 23:05:55 -0600

    Added styles inline
    Styles and animations are now inline. A css file is added in case you
    don't want to use the inline styles. Just set styled parameter as false
    when initializing the plugin, and include the css file.

--- jquery.hover-marquee-0.1.js
+++ jquery.hover-marquee-0.1.js
@@ -1,19 +1,87 @@
-(function($) {
+/*global jQuery:false, global document: false*/
+(function ($) {
+    'use strict';
     var methods = {
-        'init': function(options) {
-            var settings = $.extend({
-                'direction': this.data('marquee-direction') || 'right',
-                'target': this.data('marquee-target') || '#marquee-target'
-            }, options);
+        init: function (options) {
+            var animation = false,
+                element = this[0],
+                keyframeprefix = '',
+                domPrefixes = [
+                    'Webkit',
+                    'Moz',
+                    'O',
+                    'ms'
+                ],
+                prefix  = '',
+                i = 0,
+                l = 0,
+                keyframes = '',
+                styleElement = null;
+            if (!element.style.animationName) {
+                for (i = 0, l = domPrefixes.length; i < l; i++) {
+                    if (element.style[domPrefixes[i] + 'AnimationName'] !== undefined) {
+                        prefix = domPrefixes[i];
+                        keyframeprefix = '-' + prefix.toLowerCase() + '-';
+                        animation = true;
+                        break;
+                    }
+                }
+            }
+            if (animation) {
+                keyframes = '@' + keyframeprefix + 'keyframes fadeIn { ' +
+                    '0% {' + keyframeprefix + 'transform: translateX(10%); opacity: 0; }' +
+                    '45%, 55% {' + keyframeprefix + 'transform: translateX(50%); opacity: 1; }' +
+                    '100% {' + keyframeprefix + 'transform: translateX(90%); opacity: 0; }' +
+                    '}';
+                styleElement = document.createElement('style');
+                styleElement.innerHTML = keyframes;
+                document.getElementsByTagName('head')[0].appendChild(styleElement);
+            }
             return this.each(function () {
                 var $this = $(this),
+                    settings = $.extend({
+                        direction: $this.data('marquee-direction') || 'right',
+                        target: $this.data('marquee-target') || '#marquee-target',
+                        title: $this.data('title') || 'Message',
+                        styled: true,
+                        marqueeWidth: '200'
+                    }, options),
                     data = $this.data('hover-marquee'),
-                    marquee = $('<span />', {
-                        'text': $this.attr('title')
+                    marqueeCss = {},
+                    marqueeTarget = $(settings.target),
+                    marquee = $('<div class="marquee"><span>' + settings.title + '</span></div>');
+                if (settings.direction === 'right') {
+                    marquee.addClass('marquee-right');
+                } else {
+                    marquee.addClass('marquee-left');
+                }
+                if (settings.styled) {
+                    marqueeCss['-webkit-animation-name'] = marqueeCss['-moz-animation-name'] =
+                        marqueeCss['-o-animation-name'] = marqueeCss['animation-name'] = 'fadeIn';
+                    marqueeCss['-webkit-animation-duration'] = marqueeCss['-moz-animation-duration'] =
+                        marqueeCss['-o-animation-duration'] = marqueeCss['animation-duration'] = '2s';
+                    marqueeCss['-webkit-animation-iteration-count'] = marqueeCss['-moz-animation-iteration-count'] =
+                        marqueeCss['-o-animation-iteration-count'] = marqueeCss['animation-iteration-count'] = '2s';
+                    marqueeCss['-webkit-animation-direction'] = marqueeCss['-moz-animation-direction'] =
+                        marqueeCss['-o-animation-direction'] = marqueeCss['animation-direction'] =
+                        marquee.hasClass('marquee-right') ? 'normal' : 'reverse';
+                    marquee.css($.extend(marqueeCss, {
+                        opacity: 0,
+                        fontSize: '30px',
+                        display: 'block'
+                    })).find('span').css({
+                        display: 'inline-block',
+                        width: settings.marqueeWidth,
+                        textAlign: 'center',
+                        marginLeft: -(settings.marqueeWidth / 2)
                     });
+                    marqueeTarget.css('overflow', 'hidden');
+                }
                 if (!data) {
-                    $(settings.target).html(marquee);
-                    $(this).data('hover-marquee', {
+                    $this.on('mouseover', function () {
+                        marqueeTarget.empty().append(marquee);
+                    });
+                    $this.data('hover-marquee', {
                         'target': $this,
                         'marquee': marquee
                     });
@@ -21,13 +89,13 @@
             });
         }
     };
-    $.fn.hoverMarquee = function(method) {
+    $.fn.hoverMarquee = function (method) {
         if (methods[method]) {
-            return methods[method].apply( his, Array.prototype.slice.call(arguments, 1));
-        } else if ( typeof method === 'object' || !method) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
             return methods.init.apply(this, arguments);
         } else {
             $.error('Method ' +  method + ' does not exist on jQuery.hoverMarquee');
         }
     };
-})(jQuery);
+}(jQuery));
